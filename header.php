b80c4837c33ff4058cba40c352ce96b67a27d9b2
RafaelNercessian <rafanercessian@gmail.com>
2020-05-31 16:20:00 -0300

    Aula02-05

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/normalize.css' ?>">
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/bootstrap.css' ?>">
     <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/header.css' ?>">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() . '/css/' . $estiloPagina ?>">
 </head>
 <body <?php body_class(); ?>>
 <header class="site-header">
--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
+$estiloPagina = 'sobre_nos.css';
 require_once 'header.php';
 
-if(have_posts()):
-    while(have_posts()): the_post();
-        the_post_thumbnail();
-        the_title();
-        the_content();
-    endwhile;
+if (have_posts()):
+    ?>
+    <main class="main-sobre-nos">
+        <?php
+        while (have_posts()): the_post();
+            the_post_thumbnail('post-thumbnail',array('class' => 'imagem-sobre-nos'));
+            echo '<div class="conteudo container-alura">';
+            the_title('<h2>','</h2>');
+            the_content();
+            echo'</div>';
+        endwhile;
+        ?>
+    </main>
+<?php
 endif;
-
 require_once 'footer.php';
