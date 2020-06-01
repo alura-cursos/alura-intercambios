9289cc25ca38be56ed9f4c66ce44acd3892f7fbe
RafaelNercessian <rafanercessian@gmail.com>
2020-06-01 15:10:50 -0300

    Aula04-04

--- page-destinos.php
+++ page-destinos.php
@@ -1,13 +1,20 @@
 <?php
+$estiloPagina = 'destinos.css';
 require_once 'header.php';
 
 $args = array('post_type' => 'destinos' );
 $query = new WP_Query($args);
 if ($query->have_posts()):
+    echo '<main class="page-destinos">';
+    echo '<ul class="lista-destinos container-alura">';
     while($query->have_posts()): $query->the_post();
+        echo '<li class="col-md-3 destinos" >';
         the_post_thumbnail();
-        the_title();
+        the_title('<p class="titulo-destino">','</p>');
         the_content();
+        echo '</li>';
     endwhile;
+    echo '</ul>';
+    echo '</main>';
 endif;
 require_once 'footer.php';
