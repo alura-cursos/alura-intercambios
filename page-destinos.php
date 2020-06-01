1e3db9a3d50bc3c0c320dd0ab29ed778f0ce19ae
RafaelNercessian <rafanercessian@gmail.com>
2020-06-01 14:44:22 -0300

    Aula04-03

--- page-destinos.php
+++ page-destinos.php
@@ -1,1 +1,13 @@
-<h1>Esse é o conteúdo da página destinos</h1>
+<?php
+require_once 'header.php';
+
+$args = array('post_type' => 'destinos' );
+$query = new WP_Query($args);
+if ($query->have_posts()):
+    while($query->have_posts()): $query->the_post();
+        the_post_thumbnail();
+        the_title();
+        the_content();
+    endwhile;
+endif;
+require_once 'footer.php';
