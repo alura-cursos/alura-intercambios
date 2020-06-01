d3e0b714d2455029017c45fd590e360510234c60
RafaelNercessian <rafanercessian@gmail.com>
2020-05-31 22:34:08 -0300

    Aula04-01

--- functions.php
+++ functions.php
@@ -1,15 +1,33 @@
 <?php
 
-function alura_intercambios_adicionando_recursos_ao_tema(){
+function alura_intercambios_registrando_post_customizado()
+{
+    register_post_type('destinos',
+        array(
+            'labels' => array('name' => 'Destinos'),
+            'public' => true,
+            'menu_position' => 0,
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'menu_icon' => 'dashicons-admin-site'
+        )
+    );
+}
+add_action('init','alura_intercambios_registrando_post_customizado');
+
+function alura_intercambios_adicionando_recursos_ao_tema()
+{
     add_theme_support('custom-logo');
     add_theme_support('post-thumbnails');
 }
+
 add_action('after_setup_theme', 'alura_intercambios_adicionando_recursos_ao_tema');
 
-function alura_intercambios_registrando_menu(){
+function alura_intercambios_registrando_menu()
+{
     register_nav_menu(
         'menu-navegacao',
         'Menu navegação'
     );
 }
+
 add_action('init', 'alura_intercambios_registrando_menu');
