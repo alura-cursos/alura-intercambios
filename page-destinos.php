ca278bf924a094cfc93a362ee71a43916e705bb7
RafaelNercessian <rafanercessian@gmail.com>
2020-06-03 13:34:18 -0300

    Aula04-05

--- page-destinos.php
+++ page-destinos.php
@@ -1,7 +1,21 @@
 <?php
 $estiloPagina = 'destinos.css';
 require_once 'header.php';
-
+?>
+<form action="#" class="container-alura formulario-pesquisa-paises">
+    <h2>Conheça nossos destinos</h2>
+    <select name="paises" id="paises">
+        <option value="">--Selecione--</option>
+        <?php
+            $paises = get_terms(array('taxonomy' => 'paises'));
+            foreach($paises as $pais):?>
+                <option value="<?= $pais->name ?>"><?= $pais->name ?></option>
+            <?php endforeach;
+        ?>
+    </select>
+    <input type="submit" value="Pesquisar">
+</form>
+<?php
 $args = array('post_type' => 'destinos' );
 $query = new WP_Query($args);
 if ($query->have_posts()):
