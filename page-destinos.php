54deaed593ee1bb0ffe8721410b518b736fa507c
RafaelNercessian <rafanercessian@gmail.com>
2020-06-03 14:51:43 -0300

    Aula05-03

--- page-destinos.php
+++ page-destinos.php
@@ -2,29 +2,39 @@
 $estiloPagina = 'destinos.css';
 require_once 'header.php';
 ?>
-<form action="#" class="container-alura formulario-pesquisa-paises">
-    <h2>Conheça nossos destinos</h2>
-    <select name="paises" id="paises">
-        <option value="">--Selecione--</option>
-        <?php
+    <form action="#" class="container-alura formulario-pesquisa-paises">
+        <h2>Conheça nossos destinos</h2>
+        <select name="paises" id="paises">
+            <option value="">--Selecione--</option>
+            <?php
             $paises = get_terms(array('taxonomy' => 'paises'));
-            foreach($paises as $pais):?>
+            foreach ($paises as $pais):?>
                 <option value="<?= $pais->name ?>"><?= $pais->name ?></option>
             <?php endforeach;
-        ?>
-    </select>
-    <input type="submit" value="Pesquisar">
-</form>
+            ?>
+        </select>
+        <input type="submit" value="Pesquisar">
+    </form>
 <?php
-$args = array('post_type' => 'destinos' );
+
+$paisSelecinado = array(array(
+    'taxonomy' => 'paises',
+    'field' => 'name',
+    'terms' => $_GET['paises']
+));
+
+$args = array(
+    'post_type' => 'destinos',
+    'tax_query' => $paisSelecinado
+);
 $query = new WP_Query($args);
 if ($query->have_posts()):
     echo '<main class="page-destinos">';
     echo '<ul class="lista-destinos container-alura">';
-    while($query->have_posts()): $query->the_post();
+    while ($query->have_posts()): $query->the_post();
         echo '<li class="col-md-3 destinos" >';
         the_post_thumbnail();
-        the_title('<p class="titulo-destino">','</p>');
+        the_title('<p class="titulo-destino">', '</p>');
         the_content();
         echo '</li>';
     endwhile;
